a1cea8708a3cd71c8f1882bfd91330c78fca6076
Denis Mylnikov <>
2019-05-26 12:04:47 +0200

    deployment stuff

---
+++ build/precache-manifest.6ef2d9300e961abce8a7a6658838fe5f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/WL-Estimator/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "4ca8f79f8918becff048",
+    "url": "/WL-Estimator/static/css/main.237b6bdb.chunk.css"
+  },
+  {
+    "revision": "5fce7c1238bad823bee064bd127fe4cd",
+    "url": "/WL-Estimator/static/media/Framework7Icons-Regular.5fce7c12.woff"
+  },
+  {
+    "revision": "34402027cedd2c0fbdb6",
+    "url": "/WL-Estimator/static/js/1.34402027.chunk.js"
+  },
+  {
+    "revision": "306b7b2070d1feaf5046",
+    "url": "/WL-Estimator/static/js/runtime~main.306b7b20.js"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/WL-Estimator/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/WL-Estimator/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "4ca8f79f8918becff048",
+    "url": "/WL-Estimator/static/js/main.4ca8f79f.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/WL-Estimator/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "28db829912bb402df5b1e6fb16f1908c",
+    "url": "/WL-Estimator/static/media/Framework7Icons-Regular.28db8299.eot"
+  },
+  {
+    "revision": "19158ff09eb907432a7ccacd074ef9d5",
+    "url": "/WL-Estimator/static/media/Framework7Icons-Regular.19158ff0.woff2"
+  },
+  {
+    "revision": "111371010ef70bbcf7e6968abc8e2fb2",
+    "url": "/WL-Estimator/static/media/Framework7Icons-Regular.11137101.ttf"
+  },
+  {
+    "revision": "34402027cedd2c0fbdb6",
+    "url": "/WL-Estimator/static/css/1.6e902da4.chunk.css"
+  },
+  {
+    "revision": "be3449064eaa2b3f347c980b9edc7740",
+    "url": "/WL-Estimator/index.html"
+  }
+];
